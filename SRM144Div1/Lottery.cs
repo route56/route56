091b11e76282d9ce77fa180c98f596d2f3f1898a
Route56_cp <SND\Route56_cp>
2011-04-05 17:43:17 +0000

    SRM144Div1 550

---
+++ SRM144Div1/Lottery.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace SRM144Div1
+{
+	public class Lottery
+	{
+		public class LotteryRule: IComparable<LotteryRule>
+		{
+			public String Name { get; set; }
+			public double Choices { get; set; }
+
+			public double Blanks { get; set; }
+
+			public bool Sorted { get; set; }
+
+			public bool Unique { get; set; }
+
+			public double LotteryCount { get; set; }
+
+			internal double GetLotteryCount()
+			{
+				double result = 0;
+				if (Sorted)
+				{
+					if (Unique)
+					{
+						result = AlgoSortedAndUnique();
+					}
+					else
+					{
+						result = AlgoSortedAndNotUnique();
+					}
+				}
+				else
+				{
+					if (Unique)
+					{
+						result = AlgoUnSortedAndUnique();
+					}
+					else
+					{
+						result = AlgoUnSortedAndNotUnique();
+					}
+				}
+
+				return result;
+			}
+
+			private double AlgoUnSortedAndNotUnique()
+			{
+				// 10, 2 => 10*10
+				// 10, 3 => 10*10*10
+				return Math.Pow(Choices, Blanks);
+			}
+
+			private double AlgoUnSortedAndUnique()
+			{
+				//10*9
+				// 10, 3 => 10*9*8
+				//result = Factorial(Choices) / Factorial(Blanks);
+				double result = 1;
+
+				Debug.Assert(Choices >= Blanks);
+				for (double i = Choices; i > Choices - Blanks; i--)
+				{
+					result *= i;
+				}
+
+				return result;
+			}
+
+			private double AlgoSortedAndNotUnique()
+			{
+				// 10+9+...1
+				// (10+9+...1)*10+...
+				// Use DP A(n,r) = Sigma A(i, r-1) i = 1-> n : Recurrence relation. Generating fn?
+				double result = 0;
+				double[,] cached = InitializeCache();
+
+				result = GeneratingFunction(cached, (long)Choices, (long)Blanks, false);
+
+				return result;
+			}
+
+			private double[,] InitializeCache()
+			{
+				double[,] cached = new double[(long)Choices + 1, (long)Blanks + 1];
+
+				for (int i = 0; i < Choices; i++)
+				{
+					for (int j = 0; j < Blanks; j++)
+					{
+						cached[i, j] = 0;
+					}
+				}
+				return cached;
+			}
+
+			private double GeneratingFunction(double[,] cached, long nn, long rr, bool isUnique)
+			{
+				double result = 0;
+
+				if (cached[nn, rr] != 0)
+				{
+					result = cached[nn, rr];
+				}
+				else
+				{
+					if (rr == 1)
+					{
+						result = nn;
+					}
+					else
+					{
+						long upperLimit = isUnique ? nn - 1: nn;
+
+						for (long i = 1; i <= upperLimit; i++)
+						{
+							result += GeneratingFunction(cached, i, rr - 1, isUnique);
+						}
+					}
+
+					cached[nn, rr] = result;
+				}
+
+				return result;
+			}
+
+			private double AlgoSortedAndUnique()
+			{
+				// 9+8+...1
+				// Use DP A(n,r) = Sigma A(i, r-1) i = 1 -> n- 1 : Recurrence relation. Generating fn?
+				double result = 0;
+				double[,] cached = InitializeCache();
+
+				result = GeneratingFunction(cached, (long)Choices, (long)Blanks, true);
+
+				return result;
+			}
+
+			public int CompareTo(LotteryRule other)
+			{
+				double dblResult = this.LotteryCount - other.LotteryCount;
+				if (dblResult == 0) // to break the tie
+				{
+					dblResult = this.Name.CompareTo(other.Name);
+				}
+
+				// dblResult NEEDED as int overflow is caused for high values leading to wrong sort order!
+				int result = 0;
+
+				if (dblResult > 0)
+					result = 1;
+				else if (dblResult == 0)
+					result = 0;
+				else
+					result = -1;
+
+				return result;
+			}
+		}
+
+
+		public string[] sortByOdds(string[] rules)
+		{
+			List<LotteryRule> ruleList = new List<LotteryRule>(rules.Length);
+
+			TransformInput(rules, ruleList);
+
+			foreach (LotteryRule item in ruleList)
+			{
+				item.LotteryCount = item.GetLotteryCount();
+			}
+
+			ruleList.Sort();
+
+			string[] results = new string[ruleList.Count];
+
+			int i = 0;
+			foreach (LotteryRule item in ruleList)
+			{
+				results[i] = item.Name;
+				i++;
+			}
+
+			return results;
+		}
+
+		private static void TransformInput(string[] rules, List<LotteryRule> ruleList)
+		{
+			foreach (string item in rules)
+			{
+				string[] str = item.Split(':');
+				Debug.Assert(str.Length == 2);
+				string[] data = str[1].Trim().Split(' ');
+				Debug.Assert(data.Length == 4);
+
+				LotteryRule rule = new LotteryRule()
+				{
+					Name = str[0],
+					Choices = Double.Parse(data[0]),
+					Blanks = Double.Parse(data[1]),
+					Sorted = data[2].CompareTo("T") == 0,
+					Unique = data[3].CompareTo("T") == 0
+				};
+
+				ruleList.Add(rule);
+			}
+		}
+	}
+}
---
+++ SRM144Div1Test/LotteryTest.cs
@@ -0,0 +1,113 @@
+﻿using SRM144Div1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestHelpers;
+
+namespace SRM144Div1Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for LotteryTest and is intended
+    ///to contain all LotteryTest Unit Tests
+    ///</summary>
+	[TestClass()]
+	public class LotteryTest
+	{
+
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		public void sortByOddsTestRun(string[] rules, string[] expected)
+		{
+			Lottery target = new Lottery();
+			string[] actual;
+			actual = target.sortByOdds(rules);
+			Assert.IsTrue(StringHelpers.AreEqualStringsArrays(expected, actual));
+		}
+
+		/// <summary>
+		///A test for sortByOdds
+		///</summary>
+		[TestMethod()]
+		public void sortByOdds()
+		{
+			sortByOddsTestRun(
+				new string[]
+				{
+					"PICK ANY TWO: 10 2 F F"
+					,"PICK TWO IN ORDER: 10 2 T F"
+					,"PICK TWO DIFFERENT: 10 2 F T"
+					,"PICK TWO LIMITED: 10 2 T T"
+				},
+				new string[]
+				{
+					"PICK TWO LIMITED",
+					"PICK TWO IN ORDER",
+					"PICK TWO DIFFERENT",
+					"PICK ANY TWO"
+				});
+
+			sortByOddsTestRun(
+				new string[]
+				{"INDIGO: 93 8 T F",
+ "ORANGE: 29 8 F T",
+ "VIOLET: 76 6 F F",
+ "BLUE: 100 8 T T",
+ "RED: 99 8 T T",
+ "GREEN: 78 6 F T",
+ "YELLOW: 75 6 F F"}
+				,new string[] { "RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "VIOLET" }
+				);
+
+			sortByOddsTestRun(new string[] { }, new string[] { });
+		}
+	}
+}
