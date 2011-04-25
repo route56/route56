246fac178d4f8e9497a8e79938b44125608e5733
Route56_cp <SND\Route56_cp>
2011-04-25 20:12:22 +0000

    SRM502Div2 250

---
+++ SRM502Div2/TheProgrammingContestDivTwo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace SRM502Div2
+{
+	public class TheProgrammingContestDivTwo
+	{
+		public int[] find(int T, int[] requiredTime)
+		{
+			Array.Sort(requiredTime);
+
+			for (int i = 1; i < requiredTime.Length; i++)
+			{
+				requiredTime[i] += requiredTime[i - 1];
+			}
+
+			int[] result = { 0, 0 };
+
+			if (T < requiredTime[0])
+			{
+				return result;
+			}
+			else if (T >= requiredTime[requiredTime.Length - 1])
+			{
+				result[0] = requiredTime.Length;
+				for (int i = 0; i < requiredTime.Length; i++)
+				{
+					result[1] += requiredTime[i];
+				}
+				return result;
+			}
+
+			int? found = null;
+			for (int i = 0; i < requiredTime.Length; i++)
+			{
+				if (requiredTime[i] > T)
+				{
+					found = i - 1;
+					break;
+				}
+			}
+
+			Debug.Assert(found.HasValue, "should have value as other checks already done");
+
+			result[0] = found.Value + 1;
+
+			for (int i = 0; i <= found.Value; i++)
+			{
+				result[1] += requiredTime[i];
+			}
+
+			return result;
+		}
+	}
+}
---
+++ TestSRM502Div2/TheProgrammingContestDivTwoTest.cs
@@ -0,0 +1,130 @@
+﻿using SRM502Div2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestSRM502Div2
+{
+
+
+	/// <summary>
+	///This is a test class for TheProgrammingContestDivTwoTest and is intended
+	///to contain all TheProgrammingContestDivTwoTest Unit Tests
+	///</summary>
+	[TestClass()]
+	public class TheProgrammingContestDivTwoTest
+	{
+
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		/// <summary>
+		///A test for find
+		///</summary>
+		[TestMethod()]
+		public void findTest()
+		{
+			//0)
+
+
+			RunFindTestFor(74,
+			new int[] { 47 },
+				//Returns: 
+						new int[] { 1, 47 },
+			"They can solve the task. solved will be 1 and penalty will be 47.");
+			//1)
+
+
+			RunFindTestFor(74,
+			new int[] { 4747 },
+				//Returns: 
+						new int[] { 0, 0 },
+			"They don't have time enough to solve the task.");
+			//2)
+
+
+			RunFindTestFor(47,
+			new int[] { 8, 5 },
+				//Returns: 
+						new int[] { 2, 18 },
+			"The order is important. If they solve task 0 first and task 1 second, solved will be 2 and penalty will be 21. If they solve task 1 first and task 0 second, solved will be 2 and penalty will be 18.");
+			//3)
+
+
+			RunFindTestFor(47,
+			new int[] { 12, 3, 21, 6, 4, 13 },
+				//Returns: 
+						new int[] { 5, 86 }, "");
+
+			//4)
+
+
+			RunFindTestFor(58,
+			new int[] { 4, 5, 82, 3, 4, 65, 7, 6, 8, 7, 6, 4, 8, 7, 6, 37, 8 },
+				//Returns: 
+						new int[] { 10, 249 }, "");
+
+			//5)
+
+
+			RunFindTestFor(100000,
+			new int[] { 100000 },
+				//Returns: 
+						new int[] { 1, 100000 }, "");
+		}
+
+		private void RunFindTestFor(int T, int[] requiredTime, int[] expected, string p_4)
+		{
+			TheProgrammingContestDivTwo target = new TheProgrammingContestDivTwo(); // TODO: Initialize to an appropriate value
+			int[] actual;
+			actual = target.find(T, requiredTime);
+			CollectionAssert.AreEqual(expected, actual, p_4);
+		}
+	}
+}
