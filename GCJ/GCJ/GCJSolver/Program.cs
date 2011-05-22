382ab95e8b0304fb2ed4148f9d0e61a15024c638
Route56_cp <SND\Route56_cp>
2011-05-22 08:21:03 +0000

    WTF Rank1 soln produces same output as mine. GCJ is buggy :X

--- GCJ/GCJ/GCJSolver/Program.cs
+++ GCJ/GCJ/GCJSolver/Program.cs
@@ -7,127 +7,73 @@ namespace GCJSolver
 {
 	public class Program
 	{
-		class Data
+		int N;
+		string[] s = new string[110];
+		double[] WP = new double[110];
+		double[,] WP2 = new double[110,110];
+		double[] OWP= new double[110];
+		double[] OOWP=new double[110];
+		double[] RPI=new double[110];
+
+		public void DoIt()
 		{
-			public double RPI { get; set; }
-			public double WP { get; set; }
-			public double OWP { get; set; }
-			public double OOWP { get; set; }
-			public double Win { get; set; }
-			public double Count { get; set; }
-		}
+			N = Int32.Parse(Console.ReadLine());
+
+			for (int ii = 0; ii < N; ii++)
+			{
+				s[ii] = Console.ReadLine();
+			}
+
+			for (int i = 0; i < N; i++)
+			{
+				for (int j = 0; j < N+1; j++)
+				{
+					double sum = 0.0, cnt = 0.0;
+
+					for (int k = 0; k < N; k++)
+					{
+						if(k != j && s[i][k] != '.')
+						{
+							cnt += 1.0;
+							if(s[i][k] == '1') sum += 1.0;
+						}
+					}
+					if(j < N) WP2[i,j] = sum / cnt; else WP[i] = sum / cnt;
+				}
+			}
+	
+	for (int i = 0; i < N; i++)
+	{
+	    double sum = 0.0, cnt = 0.0;
+	    for (int j = 0; j < N; j++) if(s[i][j] != '.')
+		{
+	        cnt += 1.0;
+	        sum += WP2[j,i];
+	    }
+	    OWP[i] = sum / cnt;
+	}
+	
+	for (int i = 0; i < N; i++){
+	    double sum = 0.0, cnt = 0.0;
+	    for (int j = 0; j < N; j++) if(s[i][j] != '.'){
+	        cnt += 1.0;
+	        sum += OWP[j];
+	    }
+	    OOWP[i] = sum / cnt;
+	}
+	
+	for (int i = 0; i < N; i++) RPI[i] = 0.25 * WP[i] + 0.5 * OWP[i] + 0.25 * OOWP[i];
+	for (int i = 0; i < N; i++) Console.WriteLine(RPI[i]);
+	    }
 
 		public static void Main(string[] args)
 		{
-			string input = Console.ReadLine();
-			for (int caser = 0; caser < Int32.Parse(input); caser++)
+			int caser = Int32.Parse(Console.ReadLine());
+			for (int i = 0; i < caser; i++)
 			{
-				string ip = Console.ReadLine();
-				int n = Int32.Parse(ip);
-				bool?[,] grid = new bool?[n, n];
-
-				for (int row = 0; row < n; row++)
-				{
-					string ip2 = Console.ReadLine();
-					for (int col = 0; col < n; col++)
-					{
-						switch (ip2[col])
-						{
-							case '1':
-								grid[row, col] = true;
-								break;
-							case '0':
-								grid[row, col] = false;
-								break;
-							default:
-								grid[row, col] = null;
-								break;
-						}
-					}
-				}
-
-				double[] res = Program.GetRPI(grid, n);
-
-				Console.WriteLine("Case #{0}:", caser+1);
-
-				foreach (double item in res)
-				{
-					Console.WriteLine(item);
-				}
+				Console.WriteLine("Case #{0}:",i);
+				new Program().DoIt();
 			}
 		}
-
-		private static double[] GetRPI(bool?[,] grid, int n)
-		{
-			List<Data> dt = new List<Data>(n);
-
-			for (int i = 0; i < n; i++)
-			{
-				dt.Add(new Data());
-				dt[i].Count = 0;
-				dt[i].Win = 0;
-				for (int j = 0; j < n; j++)
-				{
-					if (grid[i,j].HasValue)
-					{
-						dt[i].Count++;
-						dt[i].Win += (grid[i, j].Value ? 1 : 0);
-					}
-				}
-
-				dt[i].WP = dt[i].Win / dt[i].Count;
-			}
-
-			for (int i = 0; i < n; i++)
-			{
-				// D
-				dt[i].OWP = 0;
-				for (int j = 0; j < n; j++)
-				{
-					//= dt[].WP;
-					// B
-					if (grid[i, j].HasValue)
-					{
-						if (grid[i, j].Value)
-						{
-							dt[i].OWP += (dt[j].Win / (dt[j].Count - 1));// b.win / (b.Count -1)
-						}
-						else
-						{
-							dt[i].OWP += ((dt[j].Win -1)/(dt[j].Count - 1));// c.Win - 1 / c.Count -1
-						}
-					}
-				}
-
-				dt[i].OWP = dt[i].OWP / dt[i].Count;
-			}
-
-			for (int i = 0; i < n; i++)
-			{
-				dt[i].OOWP = 0;
-				for (int j = 0; j < n; j++)
-				{
-					if (grid[i, j].HasValue)
-					{
-						dt[i].OOWP += dt[j].OWP;
-					}
-				}
-
-				dt[i].OOWP = dt[i].OOWP/ dt[i].Count;
-			}
-
-			for (int i = 0; i < n; i++)
-			{
-				dt[i].RPI = 0.25 * dt[i].WP + 0.50 * dt[i].OWP + 0.25 * dt[i].OOWP;
-			}
-
-			double[] res = new double[n];
-			for (int i = 0; i < n; i++)
-			{
-				res[i] = dt[i].RPI;
-			}
-
-			return res;
 		}
-	}
 }
