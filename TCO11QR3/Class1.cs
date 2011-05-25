3c2630034888c6cf6fd1a2d5fa27456003cffb3e
Route56_cp <SND\Route56_cp>
2011-05-25 02:18:10 +0000

    not working

--- TCO11QR3/Class1.cs
+++ TCO11QR3/Class1.cs
@@ -7,11 +7,22 @@ namespace TCO11QR3
 {
 	public class AllButOneDivisor
 	{
+		int[] PrimeNo = { 2, 3, 5, 7, 11, 13 };
+
 		public int getMinimum(int[] divisors)
 		{
 			Array.Sort(divisors);
 
-			long lcm = divisors[0];
+			for (int i = divisors.Length - 1; i >= 0; i--)
+			{
+				if (IsPrime(divisors[i]))
+				{
+					return LCM(divisors, i);
+				}
+			}
+
+
+			int lcm = divisors[0];
 			for (int i = 1; i < divisors.Length - 1; i++)
 			{
 				lcm = LCM(lcm, divisors[i]);
@@ -25,7 +36,7 @@ namespace TCO11QR3
 			int ans = 0;
 			for (int num = 1; num < Int32.MaxValue; num++) // refactor this
 			{
-				if ((num*lcm)%divisors[divisors.Length - 1] != 0)
+				if ((num * lcm) % divisors[divisors.Length - 1] != 0)
 				{
 					ans = (int)(num * lcm);
 					break;
@@ -33,14 +44,43 @@ namespace TCO11QR3
 			}
 
 			return ans;
+
+
 		}
 
-		private long LCM(long a, long b)
+		private bool IsPrime(int p)
+		{
+			foreach (var prime in PrimeNo)
+			{
+				if (p % prime == 0 && p / prime == 1)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private int LCM(int[] divisors, int inp)
+		{
+			int lcm = divisors[0];
+			for (int i = 1; i < divisors.Length; i++)
+			{
+				if (i != inp)
+				{
+					lcm = LCM(lcm, divisors[i]);
+				}
+			}
+
+			return lcm;
+		}
+
+		private int LCM(int a, int b)
 		{
 			return (a / GCD(a,b)) * b;
 		}
 
-		public long GCD(long a, long b) // zero case not handled
+		public int GCD(int a, int b) // zero case not handled
 		{
 			if (b == 0)
 			{
