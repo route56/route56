b5b109799963cb909199ab08930798e794275dd3
Route56_cp <SND\Route56_cp>
2011-05-25 01:54:31 +0000

    inprogress

---
+++ TCO11QR3.Test/AllButOneDivisorTest.cs
@@ -0,0 +1,109 @@
+﻿using TCO11QR3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TCO11QR3.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for AllButOneDivisorTest and is intended
+    ///to contain all AllButOneDivisorTest Unit Tests
+    ///</summary>
+	[TestClass()]
+	public class AllButOneDivisorTest
+	{
+
+
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		/// <summary>
+		///A test for getMinimum
+		///</summary>
+		[TestMethod()]
+		public void getMinimumTest()
+		{
+			RunGMTFor(
+new int[] {2,3,5},
+6,
+"There are many possible values for n in this case. For example: 6, 15, 75 and 12. 6 is the smallest of them.");
+
+    RunGMTFor(
+new int[] {2,4,3,9},
+12,"");
+
+			RunGMTFor(
+new int[] {3,2,6},
+-1,
+"Every multiple of 3 and 2 is also a multiple of 6. Every multiple of 6 is also a multiple of 2 and 3.  Therefore, a number that is a multiple of exactly 2 out of the three elements in this array cannot exist.");
+		}
+
+		[TestMethod()]
+		public void getMinimumTest2()
+		{
+			RunGMTFor(
+				new int[] {6,7,8,9,10},
+360,
+"");
+    RunGMTFor(
+new int[] {10,6,15},
+-1,
+			"");
+		}
+
+		private void RunGMTFor(int[] divisors, int expected, string msg)
+		{
+			AllButOneDivisor target = new AllButOneDivisor(); // TODO: Initialize to an appropriate value
+			int actual;
+			actual = target.getMinimum(divisors);
+			Assert.AreEqual(expected, actual, msg);
+		}
+	}
+}
---
+++ TCO11QR3/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCO11QR3
+{
+	public class AllButOneDivisor
+	{
+		public int getMinimum(int[] divisors)
+		{
+			Array.Sort(divisors);
+
+			long lcm = divisors[0];
+			for (int i = 1; i < divisors.Length - 1; i++)
+			{
+				lcm = LCM(lcm, divisors[i]);
+			}
+
+			if (lcm % divisors[divisors.Length - 1] == 0)
+			{
+				return -1;
+			}
+
+			int ans = 0;
+			for (int num = 1; num < Int32.MaxValue; num++) // refactor this
+			{
+				if ((num*lcm)%divisors[divisors.Length - 1] != 0)
+				{
+					ans = (int)(num * lcm);
+					break;
+				}
+			}
+
+			return ans;
+		}
+
+		private long LCM(long a, long b)
+		{
+			return (a / GCD(a,b)) * b;
+		}
+
+		public long GCD(long a, long b) // zero case not handled
+		{
+			if (b == 0)
+			{
+				return Math.Abs(a);
+			}
+			else
+			{
+				return (GCD(b, a % b));
+			}
+		}
+
+	}
+}
