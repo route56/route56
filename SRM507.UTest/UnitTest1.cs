bc640ca70c9eeefeb9958c32bdc696dd04ea7b23
Route56_cp <SND\Route56_cp>
2011-05-29 21:03:02 +0000

    CubeRoll sub solved

--- SRM507.UTest/UnitTest1.cs
+++ SRM507.UTest/UnitTest1.cs
@@ -54,5 +54,67 @@ RunStickerTestFor(new string[] {"white","gray","green","blue","yellow","red","ta
 		{
 			Assert.AreEqual(new CubeAnts().getMinimumSteps(p), p_2);
 		}
+
+		[TestMethod]
+		public void CubeRoll()
+		{
+			RunCubeRollFor(
+5,
+1,
+new int[] { 3 },
+-1,
+"There is a hole between initPos and goalPos, so the cube cannot be moved from initPos to goalPos.");
+
+			RunCubeRollFor(
+36,
+72,
+new int[] { 300, 100, 200, 400 },
+1,
+"The distance between initPos and goalPos is a perfect square, and there are no holes between them, so the cube can be moved in a single turn.");
+
+			RunCubeRollFor(
+10,
+21,
+new int[] { 38, 45 },
+2,
+"One of the optimal solutions is to move the cube to 10 -> -15 -> 21. (Note that the coordinate of the cube can be a negative integer.");
+
+
+			RunCubeRollFor(
+98765,
+4963,
+new int[] { 10, 20, 40, 30 },
+2, "");
+
+			RunCubeRollFor(
+68332,
+825,
+new int[] { 99726, 371, 67, 89210 },
+2, "");
+
+		}
+
+		private void RunCubeRollFor(int p, int p_2, int[] p_3, int p_4, string p_5)
+		{
+			throw new NotImplementedException();
+		}
+
+		[TestMethod]
+		public void IsHolePresent()
+		{
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 1, 8, true, "everyone is in between");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 8, 10, false, "upper side");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, -5, -2, false, "lower side");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 3, 4, false, "in range but no hole");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 6, 9, true, "between and higher");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 1, 3, true, "lower and between");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 5, 6, true, "start at hole");
+			RunIsHolePresentFor(new int[] { 2, 5, 7 }, 4, 5, true, "end at hole");
+		}
+
+		private void RunIsHolePresentFor(int[] holes, int x, int y, bool expected, string msg)
+		{
+			Assert.AreEqual(new CubeRoll().IsAnyHoleBetween(holes, x, y), expected, msg);
+		}
 	}
 }
---
+++ SRM507/Class3.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace SRM507
+{
+	public class CubeRoll
+	{
+		public int getMinimumSteps(int initPos, int goalPos, int[] holePos)
+		{
+			Array.Sort(holePos);
+
+			int dist = Math.Abs(goalPos - initPos);
+
+			if (dist == 0)
+			{
+				return 0;
+			}
+
+			if (IsAnyHoleBetween(holePos, initPos, goalPos))
+				return -1;
+
+			// dist = a^2 +/- b^2 +/- c^2...
+			throw new NotImplementedException();
+			
+		}
+
+		public bool IsAnyHoleBetween(int[] holePos, int x, int y)
+		{
+			Debug.Assert(x != y);
+			if (Array.BinarySearch(holePos, x) == Array.BinarySearch(holePos, y))
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		// This is also correct, but above one is simplified refactored truth!
+		//{
+		//    int xx = Array.BinarySearch(holePos, Math.Min(x,y));
+		//    int yy = Array.BinarySearch(holePos, Math.Max(x,y));
+
+		//    // source or dest is itself a hole
+		//    if (xx > 0 || yy > 0)
+		//    {
+		//        //return true;
+		//    }
+
+		//    xx = ~xx;
+		//    yy = ~yy;
+
+		//    // no element larger than x
+		//    if (xx >= holePos.Length)
+		//    {
+		//        //return false;
+		//    }
+
+		//    // no element smaller than y
+		//    if (yy == 0)
+		//    {
+		//        //return false;
+		//    }
+
+		//    if (xx == yy)
+		//    {
+		//        return false;
+		//    }
+		//    else
+		//    {
+		//        return true;
+		//    }
+		//}
+	}
+}
