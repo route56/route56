bc4972a2556510f7a80d360585551ce44ea86255
Route56_cp <SND\Route56_cp>
2012-03-02 20:24:38 +0000

    ZigZag DP

---
+++ RegexProblems/DynamicProgrammingProblems/ZigZag.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegexProblems.DynamicProgrammingProblems
+{
+	public class ZigZag
+	{
+		public int LongestZigZag(int[] sequence)
+		{
+			if (sequence.Length < 2)
+			{
+				return sequence.Length;
+			}
+
+			if (sequence[0] == sequence[1])
+			{
+				return 1;
+			}
+
+			bool goDown = sequence[0] < sequence[1];
+
+			int maxLen = 2;
+
+			for (int i = 2; i < sequence.Length; i++)
+			{
+				if (sequence[i-1] > sequence[i])
+				{
+					if (goDown)
+					{
+						goDown = false;
+						maxLen++;
+					}
+				}
+				else if (sequence[i-1] < sequence[i])
+				{
+					if (!goDown)
+					{
+						goDown = true;
+						maxLen++;
+					}
+				}
+			}
+
+			return maxLen;
+		}
+	}
+}
---
+++ RegexProblems/RegexProblems/UrlMatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegexProblems.RegexProblems
+{
+	class UrlMatcher
+	{
+		// ((http://)|(www\.)|(http://www\.))([^/]*\.)?regexpal\.com.*
+//matched
+//http://regexpal.com/some/more/text
+//http://regexpal.com/
+//http://www.regexpal.com/
+//www.regexpal.com/
+//http://some.regexpal.com/
+//http://foo.bar.regexpal.com/
+
+//unmatched
+//http://someregexpal.com/
+//http://regexpalbar.com/
+//http://some.com/regexpal
+//www2.regexpal.com/
+
+	}
+}
