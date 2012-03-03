9f68e72059df83c5f8affb421828855cf0119190
Route56_cp <SND\Route56_cp>
2012-03-03 13:46:00 +0000

    Bad neighbours Dynamic Prog sln

---
+++ QuickTester/BadNeighborsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegexProblems.DynamicProgrammingProblems;
+
+namespace QuickTester
+{
+	[TestClass]
+	public class BadNeighborsTest
+	{
+		[TestMethod]
+		public void KnownTests()
+		{
+			Assert.AreEqual(19,
+				new BadNeighbors().MaxDonations(new int[] { 10, 3, 2, 5, 7, 8 }),
+				"The maximum donation is 19, achieved by 10+2+7. It would be better to take 10+5+8 except that the 10 and 8 donations are from neighbors.");
+
+			Assert.AreEqual(15,
+				new BadNeighbors().MaxDonations(new int[] { 11, 15 }));
+
+			Assert.AreEqual(21,
+				new BadNeighbors().MaxDonations(new int[] { 7, 7, 7, 7, 7, 7, 7 }));
+
+			Assert.AreEqual(16,
+				new BadNeighbors().MaxDonations(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 }));
+
+			Assert.AreEqual(2926,
+				new BadNeighbors().MaxDonations(new int[] { 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,  
+  6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
+  52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72 }));
+		}
+	}
+}
---
+++ RegexProblems/DynamicProgrammingProblems/BadNeighbors.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegexProblems.DynamicProgrammingProblems
+{
+	public class BadNeighbors
+	{
+		public int MaxDonations(int[] donations)
+		{
+			int[] stateBar = new int[donations.Length];
+			int[] state = new int[donations.Length];
+
+			if (donations.Length <= 3)
+			{
+				switch(donations.Length)
+				{
+					case 1:
+						return donations[0];
+					case 2:
+						return Math.Max(donations[0], donations[1]);
+					case 3:
+						return Math.Max(Math.Max(donations[0], donations[1]), donations[2]);
+				}
+			}
+
+			stateBar[0] = 0;
+			state[0] = donations[0];
+
+			stateBar[1] = donations[1];
+			state[1] = 0;
+
+			stateBar[2] = donations[2];
+			state[2] = donations[0] + donations[2];
+
+			for (int i = 3; i < donations.Length; i++)
+			{
+				stateBar[i] = donations[i] + Math.Max(stateBar[i - 2], stateBar[i - 3]);
+				state[i] = donations[i] + Math.Max(state[i - 2], state[i - 3]);
+			}
+
+			return Math.Max(state[state.Length - 2], stateBar[stateBar.Length - 1]);
+		}
+
+		//{
+		//    int[] states = new int[donations.Length];
+
+		//    if (donations.Length <= 3)
+		//    {
+		//        throw new NotImplementedException();
+		//    }
+
+		//    states[0] = donations[0];
+		//    states[1] = donations[1];
+		//    states[2] = donations[2];
+
+		//    for (int i = 3; i < donations.Length; i++)
+		//    {
+		//        states[i] = donations[i];
+		//        states[i - 1] += states[0];
+
+		//        for (int j = 1; j < i - 1; j++)
+		//        {
+		//            if (states[j] + donations[i] > states[i])
+		//            {
+		//                states[i] = states[j] + donations[i];
+		//            }
+		//        }
+		//    }
+
+		//    int maxDon = 0;
+
+		//    for (int i = 0; i < states.Length; i++)
+		//    {
+		//        if (maxDon < states[i])
+		//        {
+		//            maxDon = states[i];
+		//        }
+		//    }
+
+		//    return maxDon;
+		//}
+	}
+}
