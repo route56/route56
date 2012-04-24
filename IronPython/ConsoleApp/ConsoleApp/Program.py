cecbabcea7c6d063b5784fc7b61c2132dd3ac63f
Route56_cp <SND\Route56_cp>
2012-04-24 17:41:19 +0000

    creating array and appending to list

--- IronPython/ConsoleApp/ConsoleApp/Program.py
+++ IronPython/ConsoleApp/ConsoleApp/Program.py
@@ -1,1 +1,22 @@
-print 'Hello World'
+#  Modify your code to create probability vectors, p, of arbitrary 
+#  size, n. Use n=5 to verify that your new solution matches 
+#  the previous one.
+
+# python create array of size n
+# http://stackoverflow.com/questions/1859864/how-to-create-an-integer-array-in-python
+
+n=5
+q=[0]*n
+
+for x in range(0, n) :
+    q[x] = 1.0/n
+
+print q
+
+p=[]
+
+for y in range(n) :
+    p.append(1./n)
+
+print p
+
