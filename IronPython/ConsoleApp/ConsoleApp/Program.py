1b8984e53c938ffaa28d4c6babc8578b0fd2d08e
Route56_cp <SND\Route56_cp>
2012-04-26 20:19:46 +0000

    python asserts, collection assert, floating comparison

--- IronPython/ConsoleApp/ConsoleApp/Program.py
+++ IronPython/ConsoleApp/ConsoleApp/Program.py
@@ -1,12 +1,16 @@
-#Program a function that returns a new distribution 
-#q, shifted to the right by U units. If U=0, q should 
-#be the same as p.
+#Modify the move function to accommodate the added 
+#probabilities of overshooting or undershooting 
+#the intended destination.
 
-p=[0, 1, 0, 0, 0]
+
 world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
+
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q=[]
@@ -18,19 +22,37 @@ def sense(p, Z):
         q[i] = q[i] / s
     return q
 
-def move(p, U):
-    #
-    #ADD CODE HERE
-    #
-    q=[]
-    for i in range(len(p)) :
-        q.append(p[(i - U) % len(p)])
-
+def move(p, u):
+    q = []
+    for i in range(len(p)):
+        q.append(p[(i-u+1)%len(p)]*pUndershoot + p[(i-u)%len(p)]*pExact + p[(i-u-1)%len(p)]*pOvershoot);
     return q
 
-print move(p, 1)
-print move(p, 3)
-print move(p, -1)
-print move(p, -3)
+def collectionAssert(e, a):
+    assert len(e) == len(a)
+    for i in range(len(e)) :
+        assert abs(e[i] - a[i]) < 0.000001
+    return
+
+#p=[0, 1, 0, 0, 0]
+#pExact = 0.8
+#pOvershoot = 0.1
+#pUndershoot = 0.1
+
+p=[0, 1, 0, 0, 0]
+e=[0,0,0.1,0.8,0.1]
+
+collectionAssert(e, move(p, 2))
+
+p=[0.2, 0.2, 0.2, 0.2, 0.2]
+e=[0.2, 0.2, 0.2, 0.2, 0.2]
+
+collectionAssert(e, move(p, 2))
+
+
+p=[0, 0.5, 0, 0.5, 0]
+e=[0.4, 0.05, 0.05, 0.4, 0.1]
+
+collectionAssert(e, move(p, 2))
 
 print 1
