e9065db5d969facab3ae20cb360fae575034f7cf
Route56_cp <SND\Route56_cp>
2012-06-20 07:16:50 +0000

    Multi dimention knapsack simplified problem algo 1

---
+++ KnapsackReloaded/MultiDimKnapsackSimplified/MultiDimKnapsackSimplified/Solver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiDimKnapsackSimplified
+{
+	public class Solver<TItem, TDim>
+	{
+		public IList<TItem> Solve(IList<TItem> items, IList<TDim> dim, Func<TItem, TDim, bool> map, IList<UInt32> weight)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	class PairData
+	{
+		public int Max { get; set; }
+		public int Index { get; set; }
+		public int[] Weight { get; set; }
+	}
+
+	public class SolverNonFancy
+	{
+		public int[] SolveNonFancy(int[,] map, int[] weight)
+		{
+			if (map.GetLength(0) != weight.Length)
+			{
+				throw new ArgumentException("map and weight's dimention doesn't match");
+			}
+
+			PairData[] space = new PairData[map.GetLength(1)];
+
+			for (int i = 0; i < map.GetLength(1); i++) // Main loop
+			{
+				PairData max = new PairData() { Index = i, Max = 1, Weight = new int[weight.Length] };
+
+				for (int k = 0; k < weight.Length; k++)
+				{
+					max.Weight[k] = map[k, i];
+				}
+
+				for (int j = 0; j < i; j++) // Max finding loop
+				{
+					bool allDimSatisfied = true;
+
+					for (int k = 0; k < weight.Length; k++) // dimention loop
+					{
+						if (space[j].Weight[k] + map[k, i] > weight[k])
+						{
+							allDimSatisfied = false;
+							break;
+						}
+					}
+
+					if (allDimSatisfied)
+					{
+						if (max.Max < space[j].Max + 1)
+						{
+							max.Max = space[j].Max + 1;
+							max.Index = j;
+						}
+					}
+				}
+
+				if (max.Index != i)
+				{
+					for (int k = 0; k < weight.Length; k++)
+					{
+						max.Weight[k] += space[max.Index].Weight[k];
+					}
+				}
+
+				space[i] = max;
+			}
+
+			PairData overallMax = new PairData() { Index = -1, Max = -1};
+
+			for (int i = 0; i < space.Length; i++)
+			{
+				if (overallMax.Max < space[i].Max)
+				{
+					overallMax.Max = space[i].Max;
+					overallMax.Index = i;
+				}
+			}
+
+			List<int> result = new List<int>();
+
+			do
+			{
+				result.Add(overallMax.Index);
+				overallMax = space[overallMax.Index];
+			} while (overallMax.Index != result.Last());
+
+			result.Sort();
+
+			return result.ToArray();
+		}
+	}
+}
---
+++ KnapsackReloaded/MultiDimKnapsackSimplified/Solver.Test/SolverTest.cs
@@ -0,0 +1,84 @@
+﻿using MultiDimKnapsackSimplified;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Solver.Test
+{
+	[TestClass()]
+	public class SolverTest
+	{
+		//public void SolveTestHelper<TItem, TDim>()
+		//{
+		//    Solver<TItem, TDim> target = new Solver<TItem, TDim>();
+		//    IList<TItem> items = null; // TODO: Initialize to an appropriate value
+		//    IList<TDim> dim = null; // TODO: Initialize to an appropriate value
+		//    Func<TItem, TDim, bool> map = null; // TODO: Initialize to an appropriate value
+		//    IList<uint> weight = null; // TODO: Initialize to an appropriate value
+		//    IList<TItem> expected = null; // TODO: Initialize to an appropriate value
+		//    IList<TItem> actual;
+		//    actual = target.Solve(items, dim, map, weight);
+		//    Assert.AreEqual(expected, actual);
+		//    Assert.Inconclusive("Verify the correctness of this test method.");
+		//}
+
+		//[TestMethod()]
+		//public void SolveTest()
+		//{
+		//    SolveTestHelper<GenericParameterHelper, GenericParameterHelper>();
+		//}
+
+		[TestMethod()]
+		public void SolveTest_BasicCases()
+		{
+			int[,] map = 
+			{
+				{1, 1, 0},
+				{0, 1, 1}
+			};
+
+			int[] weight = 
+			{
+				1,
+				1
+			};
+
+			int[] expected = { 0, 2 };
+
+			SolverNonFancy target = new SolverNonFancy();
+
+			CollectionAssert.AreEqual(expected, target.SolveNonFancy(map, weight));
+
+			weight = new int[] { 2, 2 };
+
+			var actual = target.SolveNonFancy(map, weight);
+			Assert.AreEqual(2, actual.Length);
+			CollectionAssert.AreNotEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void SolveTest_5X3Case()
+		{
+			int[,] map = 
+			{
+				{1, 1, 0, 1, 1},
+				{0, 1, 1, 1, 0},
+				{0, 1, 1, 0, 0},
+			};
+
+			int[] weight = 
+			{
+				3,
+				2,
+				1
+			};
+
+			int[] expected = { 0, 2, 3, 4};
+
+			SolverNonFancy target = new SolverNonFancy();
+			var actual = target.SolveNonFancy(map, weight);
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}
