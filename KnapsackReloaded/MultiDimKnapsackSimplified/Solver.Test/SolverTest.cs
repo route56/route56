93f063c1df4dadcb0097e5c454d7083e10b01760
Route56_cp <SND\Route56_cp>
2012-06-22 18:53:51 +0000

    More failing cases with diff = 2

--- KnapsackReloaded/MultiDimKnapsackSimplified/Solver.Test/SolverTest.cs
+++ KnapsackReloaded/MultiDimKnapsackSimplified/Solver.Test/SolverTest.cs
@@ -126,7 +126,7 @@ namespace Solver.Test
 		}
 
 		[TestMethod]
-		public void FailedTestDetectedByRandom()
+		public void FailedTestDetectedByRandom1()
 		{
 			int[,] map = 
 			{
@@ -160,6 +160,88 @@ namespace Solver.Test
 
 			int[] expected =   { 1, 4, 5, 6, 7, 10, };
 							// { 0, 1, 6, 7, 10};
+
+			SolverNonFancy target = new SolverNonFancy();
+			var actual = target.SolveNonFancy(map, weight);
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void FailedTestDetectedByRandom2()
+		{
+			int[,] map = {
+	{1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, },
+	{0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, },
+	{1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, },
+	{1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, },
+	{0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, },
+	{1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, },
+	{1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, },
+};
+			int[] weight = {
+15, 
+8, 
+3, 
+13, 
+10, 
+3, 
+10, 
+};
+			int[] expected = {
+1, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, };
+//Diff = 2
+//a
+//{int[10]}
+//    [0]: 0
+//    [1]: 1
+//    [2]: 2
+//    [3]: 3
+//    [4]: 4
+//    [5]: 5
+//    [6]: 9
+//    [7]: 10
+//    [8]: 11
+//    [9]: 12
+
+			SolverNonFancy target = new SolverNonFancy();
+			var actual = target.SolveNonFancy(map, weight);
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void FailedTestDetectedByRandom3()
+		{
+				int[,] map = {
+	{1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, },
+	{1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, },
+	{0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, },
+	{0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, },
+	{0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, },
+	{0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, },
+	{0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, },
+	{1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, },
+};
+int[] weight = {
+14, 
+11, 
+11, 
+2, 
+6, 
+6, 
+1, 
+4, 
+};
+int[] expected = {
+0, 2, 3, 4, 5, 6, 7, 8, };
+//Diff = 2
+//a
+//{int[6]}
+//    [0]: 0
+//    [1]: 1
+//    [2]: 2
+//    [3]: 3
+//    [4]: 4
+//    [5]: 5
 
 			SolverNonFancy target = new SolverNonFancy();
 			var actual = target.SolveNonFancy(map, weight);
