50f8297ba085287986834f4a024f0ce4aa3bd24e
Route56_cp <SND\Route56_cp>
2012-06-29 15:44:32 +0000

    interpreting for foreach and xpath token

--- XSLT/XmlToTeX/XmlToTeX/Converter.cs
+++ XSLT/XmlToTeX/XmlToTeX/Converter.cs
@@ -2,10 +2,95 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace XmlToTeX
 {
+	public class Keywords
+	{
+		public string XPath { get; set; }
+		public string ForEach { get; set; }
+	}
+
 	public class Converter
 	{
+		public string Convert(string source, IDictionary<string, IEnumerable<string>> data, Keywords keyword)
+		{
+			StringBuilder sb = new StringBuilder();
+
+			string[] splitSource;
+
+			if (string.IsNullOrWhiteSpace(keyword.ForEach))
+			{
+				splitSource = new string[] { source };
+			}
+			else
+			{
+				splitSource = source.Split(new string[] { keyword.ForEach }, StringSplitOptions.None);
+			}
+
+			for (int i = 0; i < splitSource.Length; i++)
+			{
+				if (i % 2 == 0)
+				{
+					// Simple Add
+					string[] xpathSplit = splitSource[i].Split(new string[] { keyword.XPath }, StringSplitOptions.None);
+
+					for (int j = 0; j < xpathSplit.Length; j++)
+					{
+						if (j % 2 ==0)
+						{
+							sb.Append(xpathSplit[j]);
+						}
+						else
+						{
+							sb.Append(data[xpathSplit[j]].First());
+						}
+					}
+				}
+				else
+				{
+					string[] xpathSplit = splitSource[i].Split(new string[] { keyword.XPath }, StringSplitOptions.None);
+
+					IList<IEnumerator<string>> listEnum = new List<IEnumerator<string>>();
+
+					// Get odd ones
+					for (int j = 1; j < xpathSplit.Length; j+=2)
+					{
+						listEnum.Add(data[xpathSplit[j]].GetEnumerator());
+					}
+
+					while (MoveNext(listEnum))
+					{
+						for (int j = 0; j < xpathSplit.Length; j++)
+						{
+							if (j % 2 == 0)
+							{
+								sb.Append(xpathSplit[j]);
+							}
+							else
+							{
+								sb.Append(listEnum[j / 2].Current);
+							}
+						}
+					}
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		private bool MoveNext(IList<IEnumerator<string>> listEnum)
+		{
+			foreach (var item in listEnum)
+			{
+				if (item.MoveNext() == false)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
 	}
 }
---
+++ XSLT/XmlToTeX/XmlToTex.Test/ConverterTest.cs
@@ -0,0 +1,123 @@
+﻿using XmlToTeX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace XmlToTex.Test
+{
+	[TestClass()]
+	public class ConverterTest
+	{
+		[TestMethod()]
+		public void ConvertTestXpathSingleLine()
+		{
+			Converter target = new Converter();
+			string source = "abc$foo$def";
+
+			IDictionary<string, IEnumerable<string>> data = new Dictionary<string, IEnumerable<string>>()
+			{
+			    { "foo", new string[] {"bar"} }, 
+				{ "baz", new string[] {"qux"} }
+			};
+
+			Keywords keys = new Keywords() { XPath = "$" };
+
+			string expected = "abcbardef";
+			string actual;
+			actual = target.Convert(source, data, keys);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void ConvertTestXpathMultiLine()
+		{
+			Converter target = new Converter();
+			string source = 
+@"abc
+def$foo$ghi
+jkl";
+
+			IDictionary<string, IEnumerable<string>> data = new Dictionary<string, IEnumerable<string>>()
+			{
+			    { "foo", new string[] {"bar"} }, 
+				{ "baz", new string[] {"qux"} }
+			};
+
+			Keywords keys = new Keywords() { XPath = "$" };
+
+			string expected = 
+@"abc
+defbarghi
+jkl";
+			string actual;
+			actual = target.Convert(source, data, keys);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void ConvertTestXpathMultipleTokens()
+		{
+			Converter target = new Converter();
+			string source = "abc$foo$def$baz$ghi$foo$jkl";
+
+			IDictionary<string, IEnumerable<string>> data = new Dictionary<string, IEnumerable<string>>()
+			{
+			    { "foo", new string[] {"bar"} }, 
+				{ "baz", new string[] {"qux"} }
+			};
+
+			Keywords keys = new Keywords() { XPath = "$" };
+
+			string expected = "abcbardefquxghibarjkl";
+
+			string actual;
+			actual = target.Convert(source, data, keys);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void ConvertTestXpathMultipleKeys()
+		{
+			Converter target = new Converter();
+			string source = "$#foo$#";
+
+			IDictionary<string, IEnumerable<string>> data = new Dictionary<string, IEnumerable<string>>()
+			{
+			    { "foo", new string[] {"bar"} },
+				{ "baz", new string[] {"qux"} }
+			};
+
+			Keywords keys = new Keywords() { XPath = "$#" };
+
+			string expected = "bar";
+			string actual;
+			actual = target.Convert(source, data, keys);
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod()]
+		public void ConvertTestForEachSingleLine()
+		{
+			Converter target = new Converter();
+			string source = "some#foo$baz$#text";
+
+			IDictionary<string, IEnumerable<string>> data = new Dictionary<string, IEnumerable<string>>()
+			{
+			    { "foo", new string[] {"bar"} }, 
+				{ "baz", new string[] {"qux", "fox"} }
+			};
+
+			Keywords keys = new Keywords() { XPath = "$", ForEach = "#" };
+
+			string expected = "somefooquxfoofoxtext";
+			string actual;
+			actual = target.Convert(source, data, keys);
+			Assert.AreEqual(expected, actual);
+		}
+
+		// multiline code.
+		// # $ $ # is valid but # $ # $ is not.
+		// Loop count doesn't match # $key1$ $key2$ # where key1 returns 1, key2 returns 2 items should throw exception. Count should MATCH.
+		// 
+	}
+}
