ab716f48416de6b695bf4eb8629f7c9ec0039401
Route56_cp <SND\Route56_cp>
2012-07-27 21:43:17 +0000

    month enumeration, old python code

--- IronPython/ConsoleApp/ConsoleApp/Program.py
+++ IronPython/ConsoleApp/ConsoleApp/Program.py
@@ -1,72 +1,27 @@
-colors = [['red', 'green', 'green', 'red' , 'red'],
-          ['red', 'red', 'green', 'red', 'red'],
-          ['red', 'red', 'green', 'green', 'red'],
-          ['red', 'red', 'red', 'red', 'red']]
+# -----------
+# User Instructions
+# 
+# Define two functions, straight(ranks) and flush(hand).
+# Keep in mind that ranks will be ordered from largest
+# to smallest.
 
-measurements = ['green', 'green', 'green' ,'green', 'green']
+def straight(ranks):
+    "Return True if the ordered ranks form a 5-card straight."
+    
 
+def flush(hand):
+    "Return True if all the cards have the same suit."
+    # Your code here.
+    
+def test():
+    "Test cases for the functions in poker program."
+    sf = "6C 7C 8C 9C TC".split()
+    fk = "9D 9H 9S 9C 7D".split()
+    fh = "TD TC TH 7C 7D".split()
+    assert straight([9, 8, 7, 6, 5]) == True
+    assert straight([9, 8, 8, 6, 5]) == False
+    assert flush(sf) == True
+    assert flush(fk) == False
+    return 'tests pass'
 
-motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
-
-sensor_right = 0.7
-
-p_move = 0.8
-
-def show(p):
-    for i in range(len(p)):
-        print p[i]
-
-#DO NOT USE IMPORT
-#ENTER CODE BELOW HERE
-#ANY CODE ABOVE WILL CAUSE
-#HOMEWORK TO BE GRADED
-#INCORRECT
-
-p = []
-
-def sense_and_move(colors, measurements, sensor_right,motions, p_move):
-    # initial prob distribution assuming uniform.
-    size=0
-    for i in range(len(colors)) :
-        for j in range(len(colors[i])) :
-            size += 1
-
-    q = [[1./size for j in range(len(colors[i]))] for i in range(len(colors))]
-
-    for i in range(len(measurements)) :
-        q = move(colors, q, motions[i], p_move)
-        q = sense(colors, q, measurements[i], sensor_right)
-
-    return q
-
-def sense(colors, p, Z, sensor_right):
-    pHit = sensor_right;
-    pMiss = 1 - pHit;
-
-    q = [[p[i][j] * ((Z == colors[i][j]) * pHit + (1-(Z == colors[i][j])) * pMiss) for j in range(len(p[i]))] for i in range(len(p))]
-
-    s=0
-    for i in range(len(q)) :
-        s += sum(q[i])
-
-    q = [[q[i][j] / s for j in range(len(q[i]))] for i in range(len(q))]
-
-    return q
-
-def move(colors, p, U, p_move):
-    #0,1 -> right. 0, -1 ->left 1,0 -> down -1,0 ->up. NO 1,1
-    pExact = p_move
-    pSame = 1 - p_move
-
-    q = [[pExact * p[(i-U[0]) % len(p)][(j-U[1]) % len(p[i])] + pSame * p[i][j] for j in range(len(p[i]))] for i in range(len(p))]
-
-    return q
-
-p = sense_and_move(colors, measurements, sensor_right, motions, p_move)
-
-#Your probability array must be printed 
-#with the following code.
-
-show(p)
-
-
+print test()
