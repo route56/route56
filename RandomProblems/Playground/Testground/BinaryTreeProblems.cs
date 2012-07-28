8e2c8c34764e8ed80080f1c8514b764f1408de42
Route56_cp <SND\Route56_cp>
2012-07-28 22:42:38 +0000

    Multiple ways to implement iterative / non recursive tree traversals.

--- RandomProblems/Playground/Testground/BinaryTreeProblems.cs
+++ RandomProblems/Playground/Testground/BinaryTreeProblems.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 
 namespace Testground
 {
+	[DebuggerDisplay("Value = {Value} Left = {Left.Value} Right = {Right.Value}")]
 	class Node
 	{
 		public int Value { get; set; }
@@ -13,6 +15,7 @@ namespace Testground
 		public Node Right { get; set; }
 
 		public List<int> LevelList { get; set; }
+		public bool VistedMe { get; set; }
 	}
 
 	class BSTFlatteningProblem
@@ -416,6 +419,354 @@ The idea is to figure out the height of all leaf nodes and then choose all the n
 			Assert.AreEqual(3 + 5 + 6, target.SumNodesBeforeLeafLevel(root, 0));
 
 			Assert.AreEqual(0, target.SumNodesBeforeLeafLevel(root, 4));
+		}
+	}
+
+	class TreeTraversal
+	{
+		#region inorder
+		internal void InorderRecursive(Node node, IList<int> toAdd)
+		{
+			if (node != null)
+			{
+				InorderRecursive(node.Left, toAdd);
+				toAdd.Add(node.Value);
+				InorderRecursive(node.Right, toAdd);
+			}
+		}
+
+		// http://stackoverflow.com/questions/2116662/help-me-understand-inorder-traversal-without-using-recursion
+		internal void InorderIterativeSO1(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Node>();
+
+			while (stack.Count > 0 || node != null)
+			{
+				if (node != null)
+				{
+					stack.Push(node);
+					node = node.Left;
+				}
+				else
+				{
+					node = stack.Pop();
+					toAdd.Add(node.Value);
+					node = node.Right;
+				}
+			}
+		}
+
+		enum FunctionState
+		{
+			Entered,
+			AfterLeftTraversal
+		}
+
+		internal void InorderIterativeSO2(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Tuple<Node, FunctionState>>();
+			stack.Push(new Tuple<Node, FunctionState>(node, FunctionState.Entered)); // add in advance.
+
+			while (stack.Count > 0)
+			{
+				var state = stack.Pop();
+
+				switch (state.Item2)
+				{
+					case FunctionState.Entered:
+						if (state.Item1 != null)
+						{
+							// imp. save state
+							stack.Push(new Tuple<Node, FunctionState>(state.Item1, FunctionState.AfterLeftTraversal));
+							// recurse
+							stack.Push(new Tuple<Node, FunctionState>(state.Item1.Left, FunctionState.Entered));
+						}
+						break;
+
+					case FunctionState.AfterLeftTraversal:
+						toAdd.Add(state.Item1.Value);
+
+						stack.Push(new Tuple<Node,FunctionState>(state.Item1.Right, FunctionState.Entered));
+
+						break;
+				}
+			}
+		}
+
+		internal void InorderIterative(Node root, IList<int> toAdd)
+		{
+			if (root == null)
+			{
+				return;
+			}
+
+			var mainStack = new Stack<Node>();
+
+			mainStack.Push(root);
+
+			var dyNodeColor = new Dictionary<Node, bool>();
+
+			while (mainStack.Count > 0)
+			{
+				var current = mainStack.Pop();
+
+				if (current == null)
+				{
+					continue;
+				}
+
+				if (dyNodeColor.ContainsKey(current))
+				{
+
+					if (dyNodeColor[current])
+					{
+						toAdd.Add(current.Value);
+						dyNodeColor[current] = false;
+						mainStack.Push(current.Right);
+					}
+				}
+				else
+				{
+					mainStack.Push(current);
+					mainStack.Push(current.Left);
+					dyNodeColor.Add(current, true);
+				}
+			}
+		}
+		#endregion
+
+		#region Preorder
+
+		internal void PreorderRecursive(Node node, IList<int> toAdd)
+		{
+			if (node != null)
+			{
+				toAdd.Add(node.Value);
+				PreorderRecursive(node.Left, toAdd);
+				PreorderRecursive(node.Right, toAdd);
+			}
+		}
+
+		enum FunctionPreorder
+		{
+			Entered,
+			Left,
+		}
+
+		internal void PreorderIterative(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Tuple<Node, FunctionPreorder>>();
+
+			stack.Push(new Tuple<Node,FunctionPreorder>(node, FunctionPreorder.Entered));
+
+			while (stack.Count > 0)
+			{
+				var context = stack.Pop();
+
+				switch (context.Item2)
+				{
+					case FunctionPreorder.Entered:
+						if (context.Item1 != null)
+						{
+							toAdd.Add(context.Item1.Value);
+							stack.Push(new Tuple<Node, FunctionPreorder>(context.Item1, FunctionPreorder.Left));
+							stack.Push(new Tuple<Node, FunctionPreorder>(context.Item1.Left, FunctionPreorder.Entered));
+						}
+						break;
+
+					case FunctionPreorder.Left:
+						stack.Push(new Tuple<Node, FunctionPreorder>(context.Item1.Right, FunctionPreorder.Entered));
+						break;
+				}
+			}
+		}
+
+		internal void PreorderIterativeOptimized(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Node>();
+
+			while (stack.Count > 0 || node != null)
+			{
+				if (node != null)
+				{
+					toAdd.Add(node.Value);
+
+					stack.Push(node);
+					node = node.Left;
+				}
+				else
+				{
+					node = stack.Pop();
+					node = node.Right;
+				}
+			}
+		}
+
+		#endregion
+
+		#region Postorder
+
+		internal void PostorderRecursive(Node node, IList<int> toAdd)
+		{
+			if (node != null)
+			{
+				PostorderRecursive(node.Left, toAdd);
+				PostorderRecursive(node.Right, toAdd);
+				toAdd.Add(node.Value);
+			}
+		}
+
+		enum FunctionPostorder
+		{
+			Entered,
+			Left,
+			Right
+		}
+
+		internal void PostorderIterative(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Tuple<Node, FunctionPostorder>>();
+
+			stack.Push(new Tuple<Node, FunctionPostorder>(node, FunctionPostorder.Entered));
+
+			while (stack.Count > 0)
+			{
+				var context = stack.Pop();
+
+				switch (context.Item2)
+				{
+					case FunctionPostorder.Entered:
+						if (context.Item1 != null)
+						{
+							stack.Push(new Tuple<Node, FunctionPostorder>(context.Item1, FunctionPostorder.Left));
+							stack.Push(new Tuple<Node, FunctionPostorder>(context.Item1.Left, FunctionPostorder.Entered));
+						}
+						break;
+
+					case FunctionPostorder.Left:
+						stack.Push(new Tuple<Node, FunctionPostorder>(context.Item1, FunctionPostorder.Right));
+						stack.Push(new Tuple<Node, FunctionPostorder>(context.Item1.Right, FunctionPostorder.Entered));
+						break;
+
+					case FunctionPostorder.Right:
+						toAdd.Add(context.Item1.Value);
+						break;
+				}
+			}
+		}
+
+		internal void PostorderIterativeOptimized(Node node, IList<int> toAdd)
+		{
+			var stack = new Stack<Node>();
+
+			while (stack.Count > 0 || node != null)
+			{
+				if (node != null)
+				{
+					stack.Push(node);
+					node = node.Left;
+				}
+				else
+				{
+					node = stack.Pop();
+
+					if (node.VistedMe)
+					{
+						toAdd.Add(node.Value);
+						node = null; // wow this is imp. else infinite loop.
+					}
+					else
+					{
+						node.VistedMe = true;
+						stack.Push(node);
+						node = node.Right;
+					}
+				}
+			}
+		}
+
+		#endregion
+	}
+
+	[TestClass]
+	public class TreeTraversalTest
+	{
+		[TestMethod]
+		public void InorderRecursiveIterativeMatch()
+		{
+			Node root = InitNode();
+
+			var target = new TreeTraversal();
+
+			var recurse = new List<int>();
+			var iterate = new List<int>();
+
+			target.InorderRecursive(root, recurse);
+			target.InorderIterative(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+
+			iterate.Clear();
+			target.InorderIterativeSO1(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+
+			iterate.Clear();
+			target.InorderIterativeSO2(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+		}
+
+		[TestMethod]
+		public void PreorderRecursiveIterativeMatch()
+		{
+			Node root = InitNode();
+
+			var target = new TreeTraversal();
+
+			var recurse = new List<int>();
+			var iterate = new List<int>();
+
+			target.PreorderRecursive(root, recurse);
+			target.PreorderIterative(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+
+			iterate.Clear();
+			target.PreorderIterativeOptimized(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+		}
+
+		[TestMethod]
+		public void PostorderRecursiveIterativeMatch()
+		{
+			Node root = InitNode();
+
+			var target = new TreeTraversal();
+
+			var recurse = new List<int>();
+			var iterate = new List<int>();
+
+			target.PostorderRecursive(root, recurse);
+			target.PostorderIterative(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+
+			iterate.Clear();
+			target.PostorderIterativeOptimized(root, iterate);
+			CollectionAssert.AreEqual(recurse, iterate);
+		}
+
+
+
+		private static Node InitNode()
+		{
+			Node root = new Node() { Value = 4 };
+
+			root.Left = new Node() { Value = 2 };
+			root.Right = new Node() { Value = 6 };
+
+			root.Left.Left = new Node() { Value = 1 };
+			root.Left.Right = new Node() { Value = 3 };
+
+			root.Right.Left = new Node() { Value = 5 };
+			root.Right.Right = new Node() { Value = 7 };
+			return root;
 		}
 	}
 }
