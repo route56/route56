a4ffbf577ec62d23dce3dd9d09fe43883fb2eab8
Route56_cp <SND\Route56_cp>
2012-08-05 00:47:45 +0000

    1st successful run

--- RandomProblems/Playground/Testground/StableMergeMaxProd.cs
+++ RandomProblems/Playground/Testground/StableMergeMaxProd.cs
@@ -27,9 +27,191 @@ namespace Testground
 	 */
 	class StableMergeMaxProd
 	{
-		public List<List<int>> AllStableMerge(int[] left, int lstart, int lend,
-			int[] right, int rstart, int rend)
+		public int[] Solver(int[] left, int[] right)
 		{
+			if (left.Length != right.Length)
+			{
+				throw new ArgumentException();
+			}
+
+			memoization.Clear();
+
+			NodeData ans = Solve(left, 0, right, 0);
+
+			int[] result = new int[left.Length + right.Length];
+
+			int lhead = 0;
+			int rhead = 0;
+			int head = 0;
+
+			while (ans != null)
+			{
+				if (ans.Child == null)
+				{
+					while (lhead < left.Length)
+					{
+						result[head++] = left[lhead++];
+					}
+
+					while (rhead < right.Length)
+					{
+						result[head++] = right[rhead++];
+					}
+
+					ans = null;
+				}
+				else
+				{
+					while (lhead < ans.Child.Item1)
+					{
+						result[head++] = left[lhead++];
+					}
+
+					while (rhead < ans.Child.Item2)
+					{
+						result[head++] = right[rhead++];
+					}
+
+					ans = memoization[ans.Child];
+				}
+			}
+
+			return result;
+		}
+
+		private NodeData Solve(int[] left, int lstart, int[] right, int rstart)
+		{
+			var key = Tuple.Create(lstart, rstart);
+
+			if (memoization.ContainsKey(key) == false)
+			{
+				NodeData nodeResult;
+
+				var lend = left.Length;
+				var rend = right.Length;
+
+				if (lstart == lend)
+				{
+					if (rstart == rend)
+					{
+						nodeResult = new NodeData() { Max = 0, Child = null, NeedsPairing = false };
+					}
+					else
+					{
+						nodeResult = new NodeData()
+						{
+							Max = ComputeMax(right, rstart),
+							NeedsPairing = (rend - rstart) % 2 != 0,
+							Child = null
+						};
+					}
+				}
+				else
+				{
+					if (rstart == rend)
+					{
+						nodeResult = new NodeData()
+						{
+							Max = ComputeMax(left, lstart),
+							NeedsPairing = (lend - lstart) % 2 != 0,
+							Child = null
+						};
+					}
+					else
+					{
+						var downLef = Solve(left, lstart + 1, right, rstart);
+
+						var lefResNode = new NodeData()
+						{
+							Child = Tuple.Create(lstart + 1, rstart),
+						};
+
+						if (downLef.NeedsPairing)
+						{
+							lefResNode.Max = downLef.Max + left[lstart] * right[rstart];
+							lefResNode.NeedsPairing = false;
+						}
+						else
+						{
+							lefResNode.Max = downLef.Max;
+							lefResNode.NeedsPairing = true;
+						}
+
+						var downRt = Solve(left, lstart, right, rstart + 1);
+
+						var rtResNode = new NodeData()
+						{
+							Child = Tuple.Create(lstart, rstart + 1),
+						};
+
+						if (downRt.NeedsPairing)
+						{
+							rtResNode.Max = downRt.Max + right[rstart] * left[lstart];
+							rtResNode.NeedsPairing = false;
+						}
+						else
+						{
+							rtResNode.Max = downRt.Max;
+							rtResNode.NeedsPairing = true;
+						}
+
+						if (lefResNode.Max > rtResNode.Max)
+						{
+							nodeResult = lefResNode;
+						}
+						else
+						{
+							nodeResult = rtResNode;
+						}
+					}
+				}
+
+				memoization.Add(key, nodeResult);
+			}
+
+			return memoization[key];
+		}
+
+		private int ComputeMax(int[] arr, int start)
+		{
+			if ((arr.Length - start) % 2 != 0)
+			{
+				int sum = 0;
+
+				for (int i = start; i < arr.Length - 1; i+=2)
+				{
+					sum += arr[i] * arr[i + 1];
+				}
+
+				return sum;
+			}
+			else
+			{
+				int sum = 0;
+
+				for (int i = start + 1; i < arr.Length - 1; i += 2)
+				{
+					sum += arr[i] * arr[i + 1];
+				}
+
+				return sum;
+			}
+		}
+
+		private Dictionary<Tuple<int, int>, NodeData> memoization = new Dictionary<Tuple<int, int>, NodeData>();
+
+		class NodeData
+		{
+			public int Max { get; set; }
+			public bool NeedsPairing { get; set; }
+			public Tuple<int, int> Child { get; set; }
+		}
+
+		public List<List<int>> AllStableMerge(int[] left, int lstart, int[] right, int rstart)
+		{
+			var lend = left.Length;
+			var rend = right.Length;
+
 			if (lstart == lend)
 			{
 				if (rstart == rend)
@@ -57,7 +239,7 @@ namespace Testground
 				{
 					var result = new List<List<int>>();
 
-					var downLef = AllStableMerge(left, lstart + 1, lend, right, rstart, rend);
+					var downLef = AllStableMerge(left, lstart + 1, right, rstart);
 
 					foreach (var item in downLef)
 					{
@@ -65,7 +247,7 @@ namespace Testground
 						result.Add(item);
 					}
 
-					var downRt = AllStableMerge(left, lstart, lend, right, rstart + 1, rend);
+					var downRt = AllStableMerge(left, lstart, right, rstart + 1);
 
 					foreach (var item in downRt)
 					{
@@ -82,6 +264,17 @@ namespace Testground
 	[TestClass]
 	public class StableMergeMaxProdTest
 	{
+		[TestMethod]
+		public void VerifyMaxProd()
+		{
+			var target = new StableMergeMaxProd();
+
+			// var actual = target.Solver(new int[] { 2, 1, 3 }, new int[] { 3, 7, 9 });
+			var actual = target.Solver(new int[] { 1, 100 }, new int[] { 1, 100 });
+
+			CollectionAssert.AreEqual(new int[] { 1, 1, 100, 100 }, actual);
+		}
+
 		[TestMethod]
 		public void VerifyAllPossibleMerge()
 		{
@@ -104,7 +297,7 @@ namespace Testground
 		{
 			var target = new StableMergeMaxProd();
 
-			var actual = target.AllStableMerge(a, 0, a.Length, b, 0, b.Length);
+			var actual = target.AllStableMerge(a, 0, b, 0);
 
 			Assert.AreEqual(Combination(a.Length + b.Length, a.Length), actual.Count);
 
