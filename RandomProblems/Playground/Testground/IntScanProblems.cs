a471a7770e0aef7f70bb430f5a8f8775b35ca55e
Route56_cp <SND\Route56_cp>
2012-08-08 19:17:43 +0000

    Natural number linear O(n) sort and more.

--- RandomProblems/Playground/Testground/IntScanProblems.cs
+++ RandomProblems/Playground/Testground/IntScanProblems.cs
@@ -277,4 +277,274 @@ namespace Testground
 
 		IScanProblem target = new LinearProblem();
 	}
+
+	class IntToggleProblems
+	{
+		public void Transform(int[] a)
+		{
+			for (int i = 0; i < a.Length - 1; i++)
+			{
+				if (i % 2 == 0)
+				{
+					if (a[i] < a[i + 1])
+					{
+						continue;
+					}
+					else
+					{
+						int temp = a[i];
+						a[i] = a[i + 1];
+						a[i + 1] = temp;
+					}
+				}
+				else
+				{
+					if (a[i] < a[i + 1])
+					{
+						int temp = a[i];
+						a[i] = a[i + 1];
+						a[i + 1] = temp;
+					}
+					else
+					{
+						continue;
+					}
+				}
+			}
+		}
+	}
+
+	[TestClass]
+	public class IntToggleProblemsTest
+	{
+		[TestMethod]
+		public void MyTestMethod()
+		{
+			var target = new IntToggleProblems();
+			var a = new int[] { 0, 1, 2, 3 };
+			var ex = new int[] { 0, 2, 1, 3 };
+
+			var a2 = new int[] { 2, 1, 3, 0 };
+			var ex2 = new int[] { 1, 3, 0, 2 };
+
+			target.Transform(a);
+			target.Transform(a2);
+
+			CollectionAssert.AreEqual(ex, a);
+			CollectionAssert.AreEqual(ex2, a2);
+		}
+
+		static IEnumerable<int> RandomNumberStream(Random rand)
+		{
+			while (true)
+			{
+				yield return rand.Next();
+			}
+		}
+
+		[TestMethod]
+		public void MyTestMethod2()
+		{
+			Random rand = new Random();
+
+			var a = RandomNumberStream(rand).Take(101).ToArray();
+
+			var target = new IntToggleProblems();
+
+			target.Transform(a);
+
+			for (int i = 0; i < a.Length - 1; i++)
+			{
+				if (i % 2 == 0)
+				{
+					Assert.IsTrue(a[i] < a[i + 1]);
+				}
+				else
+				{
+					Assert.IsTrue(a[i] > a[i + 1]);
+				}
+			}
+		}
+	}
+
+	/*
+	 * Natural numbers. No duplicate. Find smallest natural number missing in array size n.
+	 * XOR and index Hopping
+	 * Insertion sort, quick sort inspiration
+	 * 1,2,3,4,...100, 102, ... 
+	 * sort n scan
+	 * scan . min and max.
+	 * n numbers.
+	 * 1, n... MAX
+	 * i < n ? 
+	 */
+	class MissingNaturalNumber
+	{
+		void Swap(ref int a, ref int b)
+		{
+			int t = a;
+			a = b;
+			b = t;
+		}
+
+		internal int FindMissing(int[] arr)
+		{
+			int m = 0;
+
+			// find min
+			for (int i = 1; i < arr.Length; i++)
+			{
+				if (arr[m] > arr[i])
+				{
+					m = i;
+				}
+			}
+
+			// early exit
+			if (arr[m] != 1)
+			{
+				return 1;
+			}
+
+			int j = 0;
+			int k = arr.Length - 1;
+
+			// find quick sort index of n + 1
+			while (j < k)
+			{
+				if (arr[j] <= arr.Length + 1)
+				{
+					j++;
+				}
+				else if(arr[k] > arr.Length + 1)
+				{
+					k--;
+				}
+				else
+				{
+					Swap(ref arr[j], ref arr[k]);
+					j++;
+					k--;
+				}
+			}
+
+			// sorting natural numbers
+			SortNaturalNumbers(arr, 0, k);
+
+			// find the missing guy!
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (arr[i] != i + 1)
+				{
+					return i + 1;
+				}
+			}
+
+			return -1;
+		}
+
+		internal void SortNaturalNumbers(int[] arr, int j, int k)
+		{
+			while (j <= k)
+			{
+				if (arr[j] == j + 1)
+				{
+					j++;
+				}
+				else
+				{
+					if (arr[j] - 1 <= k)
+					{
+						Swap(ref arr[j], ref arr[arr[j] - 1]);
+					}
+					else
+					{
+						j++;
+					}
+				}
+			}
+		}
+
+		internal int FindMissingBruteForce(int[] arr)
+		{
+			Array.Sort(arr);
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				if (i + 1 != arr[i])
+				{
+					return i + 1;
+				}
+			}
+
+			return -1;
+		}
+	}
+
+	[TestClass]
+	public class MissingNaturalNumTest
+	{
+		[TestMethod]
+		public void MyTestMethod()
+		{
+			var target = new MissingNaturalNumber();
+
+			Assert.AreEqual(6, target.FindMissing(
+				Enumerable.Range(10, 3)
+				.Union(Enumerable.Range(1, 3))
+				.Union(Enumerable.Range(100, 10))
+				.Union(Enumerable.Range(4, 2))
+				.ToArray()));
+
+			Assert.AreEqual(6, target.FindMissing(
+				Enumerable.Range(100, 3)
+				.Union(Enumerable.Range(90, 3))
+				.Union(Enumerable.Range(80, 10))
+				.Union(Enumerable.Range(1, 5))
+				.ToArray()));
+
+			// passed
+			Assert.AreEqual(4, target.FindMissing(
+				Enumerable.Range(10, 3).Union(Enumerable.Range(1, 3)).ToArray()));
+
+			Assert.AreEqual(11, target.FindMissing(
+				Enumerable.Range(12, 10).Union(Enumerable.Range(1, 10)).ToArray()));
+
+			Assert.AreEqual(11, target.FindMissing(
+				Enumerable.Range(1, 10).Union(Enumerable.Range(12, 20)).ToArray()));
+
+			Assert.AreEqual(4, target.FindMissing(
+				Enumerable.Range(1, 3).Union(Enumerable.Range(10, 3)).ToArray()));
+
+		}
+
+		[TestMethod]
+		public void MyTestMethod2()
+		{
+			var target = new MissingNaturalNumber();
+
+			var arr = new int[] { 4,2,1,3 };
+
+			target.SortNaturalNumbers(arr, 0, arr.Length - 1);
+
+			CollectionAssert.AreEqual(Enumerable.Range(1, 4).ToArray(), arr);
+
+			arr = new int[] { 4, 2, 1, 3, 5, 7, 6 };
+			target.SortNaturalNumbers(arr, 0, arr.Length - 1);
+			CollectionAssert.AreEqual(Enumerable.Range(1, 7).ToArray(), arr);
+
+			arr = new int[] { 1 };
+			target.SortNaturalNumbers(arr, 0, arr.Length - 1);
+			CollectionAssert.AreEqual(Enumerable.Range(1, 1).ToArray(), arr);
+
+			arr = new int[] { };
+			target.SortNaturalNumbers(arr, 0, arr.Length - 1);
+			CollectionAssert.AreEqual(new int[] { }, arr);
+
+			arr = new int[] { 1, 2 };
+			target.SortNaturalNumbers(arr, 0, arr.Length - 1);
+			CollectionAssert.AreEqual(Enumerable.Range(1, 2).ToArray(), arr);
+		}
+	}
 }
+
